ffa60587fb04a27b37677bfcc66cdc8f419d64b2
Theo Richardson <richardson.t@ufl.edu>
2021-04-30 08:16:32 -0400

    redo rms

--- rms.py
+++ rms.py
@@ -2,24 +2,29 @@ import numpy as np
 from glob import glob
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from scipy.optimize import curve_fit
+
+def error(N,A,b):
+    return A/np.sqrt(N)+b
 
 covs = glob('cov_*_mocks.fits')
 covs.sort()
-nmocks = [10,25,50,100]
+nmocks = np.linspace(10,100,10).astype(int)
 s = np.linspace(0,40,5)
 
-cov_gt = fits.getdata('cov_gt.fits')
+cov_gt = fits.getdata('old_fits/cov_gt.fits')
 U,L,V = np.linalg.svd(cov_gt)
 cov_gt_sq = U@np.diag(np.sqrt(L))@V
 gt = np.linalg.inv(cov_gt_sq)
 
 devs = []
 
-for file in range(len(covs)):
-    matrix = fits.getdata(covs[file])
+for mock in nmocks:
+    matrix = fits.getdata(f'fits/cov_{mock}_mocks.fits')
     T = gt@matrix@gt
     devs.append(np.std(T))
 
+'''
 plt.figure()
 plt.pcolormesh(s,s,gt@cov_gt@gt)
 plt.title('T, mocks=100')
@@ -27,9 +32,13 @@ plt.xlabel(r'$S_i$'); plt.ylabel(r'$S_j$')
 plt.colorbar()
 plt.savefig('T_100.pdf',dpi=300)
 plt.close()
-
-devs.append(np.std(gt@cov_gt@gt))
+'''
+popt,pcov = curve_fit(error,nmocks,devs,p0=(2,0.5))
+#devs.append(np.std(gt@cov_gt@gt))
 plt.plot(nmocks,devs,'o')
+plt.plot(nmocks,error(nmocks,*popt),label='1/sqrt fit')
+plt.title(r'$C_{\rm gt}^{-1/2}C_{\rm mock}C_{\rm gt}^{-1/2}$')
 plt.xlabel(r'$N_{\rm mocks}$')
 plt.ylabel('RMS')
+plt.legend()
 plt.savefig('rms.pdf',dpi=300)
